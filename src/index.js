8ebf4d04b0c8821027389e7c43403995ff597461
cmnad <139146999+cmnad@users.noreply.github.com>
2023-07-11 09:24:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -151,20 +151,3 @@ document.addEventListener("keydown", handleKeyDown);
 // Initialize selected seats
 updateSelectedSeatsText();
 
-// Hide the "Open Sandbox" link
-const removeWatermark = () => {
-  const ids = [];
-  const iframes = document.body.querySelectorAll("iframe");
-  for (const iframe of iframes) {
-    if (iframe.id.startsWith("sb__open-sandbox")) ids.push(iframe.id);
-  }
-  for (const id of ids) {
-    const node = document.createElement("div");
-    node.style.setProperty("display", "none", "important");
-    node.id = id;
-    document.getElementById(id).remove();
-    document.body.appendChild(node);
-  }
-};
-
-removeWatermark();
